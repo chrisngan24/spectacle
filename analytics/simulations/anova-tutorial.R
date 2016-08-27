85a0edf6c667f558a10b57d203fd35cb66200f49
Chris Ngan <chris.ngan24@gmail.com>
2016-08-27 16:28:37 -0400

    Added simulation notebooks

--- analytics/simulations/anova-tutorial.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-ggplot(chickwts, aes(x = feed, y = weight)) +
-  geom_boxplot(fill = "grey80", colour = "blue") +
-  scale_x_discrete() + xlab("Treatment Group") +
-  ylab("Weight of Chick")
-
-summary(aov(weight ~ feed, data=chickwts))
-# significant with anova!
